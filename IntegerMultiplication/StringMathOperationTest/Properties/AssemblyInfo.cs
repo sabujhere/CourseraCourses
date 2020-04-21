e178b13c3fb2c846174963d56eb51e713b5e7cc0
sabuj.saha@sandc.com <sabuj.saha@sandc.com>
2020-04-20 22:16:12 -0500

    added unit test

---
+++ IntegerMultiplication/StringMathOperationTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StringMathOperationTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StringMathOperationTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e5b17e06-676e-4319-988a-31abbe8d3410")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ IntegerMultiplication/StringMathOperationTest/StringAdditionTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringMathOperations;
+
+namespace StringMathOperationTest
+{
+    [TestClass]
+    public class StringAdditionTest
+    {
+        [TestMethod]
+        public void AddTwoNumbers()
+        {
+            var additionHelper = new StringAdditionImpl();
+
+            var result = additionHelper.Add("5782", "7329");
+
+            Assert.AreEqual("13111",result);
+        }
+    }
+}
