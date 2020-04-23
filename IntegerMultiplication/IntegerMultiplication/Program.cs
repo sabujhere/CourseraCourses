1901d39b219857ca88c36ed845c60b3e17602ee5
Sabuj <sabuj.saha@sandc.com>
2020-04-22 21:43:44 -0500

    added sample test input

--- IntegerMultiplication/IntegerMultiplication/Program.cs
+++ IntegerMultiplication/IntegerMultiplication/Program.cs
@@ -10,8 +10,9 @@ namespace IntegerMultiplication
     {
         static void Main(string[] args)
         {
-            var intMulti = new KaratsubaImpl();
-            Console.WriteLine($"Multi: {intMulti.Multiply(5678, 1234)}");
+            var intMulti = MathOperationOnStringNumbers.KaratsubaStringMultiplication("3141592653589793238462643383279502884197169399375105820974944592",
+                "2718281828459045235360287471352662497757247093699959574966967627");
+            Console.WriteLine($"Multi: {intMulti}");
 
             //Console.WriteLine($"Multi: {intMulti.Multiply(134, 46)}");
         }
--- IntegerMultiplication/StringMathOperationTest/KaratsubaStringMultiplicationImplTest.cs
+++ IntegerMultiplication/StringMathOperationTest/KaratsubaStringMultiplicationImplTest.cs
@@ -11,12 +11,12 @@ namespace StringMathOperationTest
         public void MultiplyTwoNumbers_WithEqualNumberOfDigits()
         {
             var multiplyHelper = new KaratsubaStringMultiplicationImpl();;
-            var term1 =  "5782";
-            var term2 = "7329";
-            var exptectedResult = int.Parse(term1) * int.Parse(term2);
+            var term1 =  "3141592653589793238462643383279502884197169399375105820974944592";
+            var term2 = "2718281828459045235360287471352662497757247093699959574966967627";
+            var expectedResult ="8539734222673567065463550869546574495034888535765114961879601127067743044893204848617875072216249073013374895871952806582723184";//int.Parse(term1) * int.Parse(term2);
             var result = multiplyHelper.Multiply(term1, term2);
 
-            Assert.AreEqual(exptectedResult.ToString(),result);
+            Assert.AreEqual(expectedResult,result);
         }
     }
 }
