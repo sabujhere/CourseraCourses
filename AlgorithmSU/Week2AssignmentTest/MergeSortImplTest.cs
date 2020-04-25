cdaf699f7fa7f8fc6aa208a2160e8fc1b1c5f999
Sabuj <sabujece@gmail.com>
2020-04-25 12:03:42 -0500

    added tests

---
+++ AlgorithmSU/Week2AssignmentTest/MergeSortImplTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Week2Assignment;
+
+namespace Week2AssignmentTest
+{
+    [TestClass]
+    public class MergeSortImplTest
+    {
+        [TestMethod]
+        public void Sort_IntCollection_Success()
+        {
+            var unsortedCollection = new Collection<int>()
+            {
+                1,5,4,7,9,3
+            };
+            var sorter = new MergeSortImpl<int>();
+
+            var sortedResultActual = sorter.Sort(unsortedCollection);
+
+            CollectionAssert.AreEqual(unsortedCollection.OrderBy(item=>item).ToList(),sortedResultActual.ToList());
+        }
+    }
+}
---
+++ AlgorithmSU/Week2AssignmentTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Week2AssignmentTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Week2AssignmentTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("97e745c9-4855-49f6-8819-37a4f598041f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
