00e1d86a4aa859bf27d59d29f4fc35cb6ac0e64f
Sabuj <sabujece@gmail.com>
2020-05-02 21:31:29 -0500

    fixed bugs

--- AlgorithmSU/Week3Assignment/QuickSortImpl.cs
+++ AlgorithmSU/Week3Assignment/QuickSortImpl.cs
@@ -13,6 +13,8 @@ namespace Week3Assignment
     {
         public IPivotPositionFinder PivotPositionFinder { get; private set; }
 
+        public long ComparisonCount { get; private set; }
+
         public QuickSortImpl(IPivotPositionFinder pivotPositionFinder)
         {
             PivotPositionFinder = pivotPositionFinder;
@@ -20,6 +22,7 @@ namespace Week3Assignment
 
         public ICollection<T> Sort(ICollection<T> unsortedCollection)
         {
+            ComparisonCount = 0;
             var unsortedArray = unsortedCollection.ToArray();
             Sort(ref unsortedArray,0,unsortedArray.Length-1);
             return unsortedArray;
@@ -27,17 +30,31 @@ namespace Week3Assignment
 
         private void Sort(ref T[] unsortedArray, int startIndex, int endIndex)
         {
-            if(endIndex - startIndex <= 1) return;
+            PrintArray(unsortedArray, startIndex, endIndex);
+            if(endIndex - startIndex < 1) return;
+            ComparisonCount += endIndex - startIndex;
             var pivotIndex = PivotPositionFinder.Get(startIndex,endIndex);
-
+            if(pivotIndex != startIndex)
+                Swap(ref unsortedArray, pivotIndex,startIndex);
+            
             //partition around pivot
             int partitionIndex = Partition(ref unsortedArray, startIndex,endIndex, pivotIndex);
 
             //sort left
-            Sort(ref unsortedArray, startIndex, partitionIndex -1);
+            var leftEndIndex = partitionIndex - 1;
+            Sort(ref unsortedArray, startIndex, leftEndIndex);
             
             //sort right
-            Sort(ref unsortedArray, partitionIndex+1,unsortedArray.Length - 1);
+            Sort(ref unsortedArray, partitionIndex+1,endIndex);
+        }
+
+        private static void PrintArray(T[] unsortedArray, int startIndex, int endIndex)
+        {
+            Debug.WriteLine("Sorting: ");
+            for (int i = startIndex; i <= endIndex; i++)
+            {
+                Debug.WriteLine(unsortedArray[i]);
+            }
         }
 
 
@@ -47,16 +64,17 @@ namespace Week3Assignment
             if(!isPivotIndexValid)
                 throw new Exception($"Pivot index not valid, PivotIndex: {pivotIndex}, startIndex:{startIndex}, endIndex: {endIndex}");
 
-            if(pivotIndex != startIndex)
-                Swap(ref unsortedArray, pivotIndex,startIndex);
+           
 
             pivotIndex = startIndex;
             var pivotItem = unsortedArray[pivotIndex];
             var i = pivotIndex + 1;
             for (int j = pivotIndex+1; j <= endIndex; j++)
             {
+                
                 if (pivotItem.CompareTo(unsortedArray[j]) > 0)
                 {
+                    
                     //swap(i, j)
                     Swap(ref unsortedArray, i, j);
                     i++;
