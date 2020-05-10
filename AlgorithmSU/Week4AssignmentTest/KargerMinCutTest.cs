2c7febba058b21333ecbd68f865fdaf6039e7b33
sabujhere <sabujece@users.noreply.github.com>
2020-05-09 22:40:28 -0500

    added test project, data and sample test

---
+++ AlgorithmSU/Week4AssignmentTest/KargerMinCutTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Week4Assignment;
+
+namespace Week4AssignmentTest
+{
+    [TestClass]
+    public class KargerMinCutTest
+    {
+        [TestMethod]
+        public void CalcualteMinCutTest()
+        {
+            //Arrange
+            var fileLocation = @"..\..\TestData\SampleData_kargerMinCut.txt";
+
+            if (!File.Exists(fileLocation))
+                Assert.Fail("File not found");
+            var lines = File.ReadAllLines(fileLocation);
+            var vertices = new List<Vertex>();
+            foreach (var line in lines)
+            {
+                var lineItems = line.Split(new[] {' ', '\t'}, StringSplitOptions.RemoveEmptyEntries);
+                vertices.Add(new Vertex(int.Parse(lineItems.First()),lineItems.Skip(1).Select(adjV=>int.Parse(adjV)).ToArray()));
+            }
+            
+        }
+    }
+}
---
+++ AlgorithmSU/Week4AssignmentTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("KargerMinCutTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("KargerMinCutTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0d2e1217-84e9-4f7b-9dd3-4327c7c75435")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
