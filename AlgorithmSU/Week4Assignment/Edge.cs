7ed636f86ff568f91a6fa1226eaf994f2438d2f3
sabujhere <sabujece@users.noreply.github.com>
2020-05-09 21:43:30 -0500

    init commit Week4Assignment

---
+++ AlgorithmSU/Week4Assignment/Edge.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week4Assignment
+{
+    public class Edge:IComparable<Edge>
+    {
+        public int HeadId { get; private set; }
+
+        public int TailId { get; set; }
+
+        public Edge(int headId, int tailId)
+        {
+            HeadId = headId;
+            TailId = tailId;
+        }
+        public int CompareTo(Edge other)
+        {
+            if (ReferenceEquals(this, other)) return 0;
+            if (ReferenceEquals(null, other)) return 1;
+            var headIdComparison = HeadId.CompareTo(other.HeadId);
+            if (headIdComparison != 0) return headIdComparison;
+            return TailId.CompareTo(other.TailId);
+        }
+    }
+}
---
+++ AlgorithmSU/Week4Assignment/Vertex.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week4Assignment
+{
+    public class Vertex : IComparable<Vertex>
+    {
+        public int[] AdjacentVertexIds { get; private set; }
+        public int Id { get; private set; }
+        public Vertex Parent { get; private set; }
+        public int Rank { get; set; }
+
+        public Vertex(int id):this(id,null)
+        {
+        }
+
+        public Vertex(int id, int[] adjacentVertexIds)
+        {
+            Id = id;
+            AdjacentVertexIds = adjacentVertexIds;
+            Parent = this;
+            Rank = 0;
+        }
+
+        public int CompareTo(Vertex other)
+        {
+            if (ReferenceEquals(this, other)) return 0;
+            if (ReferenceEquals(null, other)) return 1;
+            return Id.CompareTo(other.Id);
+        }
+    }
+}
