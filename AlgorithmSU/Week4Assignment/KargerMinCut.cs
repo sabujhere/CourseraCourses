870aff4cd2383c2b31c74970afe2b3743a4f2add
sabujhere <sabujece@users.noreply.github.com>
2020-05-09 22:56:53 -0500

    init commit for Karger mincut algo

---
+++ AlgorithmSU/Week4Assignment/KargerMinCut.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week4Assignment
+{
+    public class KargerMinCut
+    {
+        public IEnumerable<Vertex> Vertices { get; private set; }
+        public IEnumerable<Edge> Edges { get; private set; }
+
+        public KargerMinCut(IEnumerable<Vertex> vertices)
+        {
+            Vertices = vertices;
+            Edges = Vertices.SelectMany(ver => ver.AdjacentVertexIds.Select(adjVId => new Edge(ver.Id, adjVId)));
+        }
+
+        public int GetCount()
+        {
+            foreach (var vertex in Vertices)
+            {
+                vertex.Parent = vertex;
+                vertex.Rank = 0;
+
+            }
+            return 0;
+        }
+    }
+}
