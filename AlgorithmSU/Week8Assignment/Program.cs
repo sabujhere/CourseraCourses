0aba657f01f14e772db6e2848b9aaebb0d92f771
sabujhere <sabujece@users.noreply.github.com>
2020-06-14 20:04:35 -0500

    final impl

--- AlgorithmSU/Week8Assignment/Program.cs
+++ AlgorithmSU/Week8Assignment/Program.cs
@@ -18,7 +18,16 @@ namespace Week8Assignment
             var stopWatch = new Stopwatch();
             stopWatch.Start();
             Console.WriteLine($"using File path: {filePath}");
-            var nodes = File.ReadAllLines(filePath).Select(long.Parse).ToArray();
+            var numbers = File.ReadAllLines(filePath).Select(long.Parse).ToList();
+
+            stopWatch.Stop();
+            var initTime = stopWatch.ElapsedMilliseconds;
+            Console.WriteLine("read and init time={0}", initTime);
+            stopWatch.Restart();
+            var result = TwoSumAlgoImpl.Calculate(-10000, 10000, numbers);
+
+            stopWatch.Stop();
+            Console.WriteLine($"running time = {stopWatch.ElapsedMilliseconds}, result={result}");
         }
     }
 }
---
+++ AlgorithmSU/Week8Assignment/TwoSumAlgoImpl.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Week8Assignment
+{
+    public class TwoSumAlgoImpl
+    {
+        public static int Calculate(int start, int finish, List<long> rwnumbers)
+        {
+            var hashSet = rwnumbers.ToHashSet();
+            int result = 0;
+            var numRange = Enumerable.Range(start, finish - start);
+            Parallel.ForEach(numRange,
+                new ParallelOptions
+                    {MaxDegreeOfParallelism = Convert.ToInt32(Math.Ceiling((Environment.ProcessorCount * 0.75) * 1.0))},
+                (currentNumber) =>
+                {
+                    foreach (var number in rwnumbers)
+                    {
+                        var y = currentNumber - number;
+                        if (hashSet.Contains(y))
+                        {
+                            Interlocked.Increment(ref result);
+                            if(result %10 == 0)
+                                Console.WriteLine($"Current result: {result}");
+
+                            return;
+                        }
+                    }
+                });
+           
+
+            return result;
+        }
+    }
+}
